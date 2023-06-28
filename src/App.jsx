1fefd930794646cee2d9d7c9367d3c7b75698ce6
Anthony Nanfito <81890820+ananfito@users.noreply.github.com>
2023-06-28 21:57:35 +0700

    add styling, create Footer compenent

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,42 @@
 import { useState, useEffet } from 'react'
 import './App.css'
 import Die from './Die.jsx'
+import { nanoid } from 'nanoid'
 
 function App() {
+  const [diceNumbers, setDiceNumbers] = useState(allNewDice())
+
+  function allNewDice() {
+    let newDice = []
+    for (let i = 0; i < 10; i++) {
+      newDice.push({
+        value: Math.floor((Math.random() * 6) + 1),
+        isHeld: false,
+        id: nanoid()
+      })
+    }
+    return newDice
+  }
+
+  const diceElements = diceNumbers.map((die) => (
+    <Die 
+      key={die.id} 
+      value={die.value} 
+      isHeld={die.isHeld}/>
+    ))
+
+  function getNewDice() {    
+    setDiceNumbers(allNewDice())
+  }
 
   return (
     <main className='container'>
       <h1>Tenzies</h1>
       <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className='dice-container'>
-        <Die value='1' />
-        <Die value='2' />
-        <Die value='3' />
-        <Die value='4' />
-        <Die value='5' />
-        <Die value='6' />
-        <Die value='1' />
-        <Die value='1' />
-        <Die value='1' />
-        <Die value='1' />
+        {diceElements}
       </div>
+      <button onClick={getNewDice} className='roll-btn'>Roll</button>
     </main>
   )
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer>
+            <p>Created by <a href="https://ananfito.github.io" target='_blank'>Anthony Nanfito</a></p>
+        </footer>
+    )
+}
+
+export default Footer
