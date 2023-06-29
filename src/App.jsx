bc33a9d325f90484a24d700372b202d91e89a48b
Anthony Nanfito <81890820+ananfito@users.noreply.github.com>
2023-06-29 16:59:44 +0700

    add functionality for game over

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { useState, useEffet } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Die from './Die.jsx'
 import { nanoid } from 'nanoid'
+import Confetti from 'react-confetti'
 
 function App() {
   const [diceNumbers, setDiceNumbers] = useState(allNewDice())
+  const [tenzies, setTenzies] = useState(false)
 
   function generateNewDie() {
     return  {
@@ -31,14 +33,20 @@ function App() {
     />
     ))
 
-  function getNewDice() {    
-    setDiceNumbers(oldDice => oldDice.map(die => {
-      if (die.isHeld) {
-        return die
-      } else {
-        return generateNewDie()
-      }
-    }))
+  function getNewDice() {   
+    if (tenzies) { 
+      setTenzies(false)
+      setDiceNumbers(allNewDice())
+    } else {
+      setDiceNumbers(oldDice => oldDice.map(die => {
+        if (die.isHeld) {
+          return die
+        } else {
+          return generateNewDie()
+        }
+      }))
+    }
+    
   }
 
   function holdDie(id) {
@@ -47,14 +55,30 @@ function App() {
     }))
   }
 
+  useEffect(() => {
+    const allHeld = diceNumbers.every(die => die.isHeld) // returns `true` when every die's isHeld = true
+    const firstValue = diceNumbers[0].value
+    const allSame = diceNumbers.every(die => die.value === firstValue) // returns `true` when every die's value is the same
+
+    if (allHeld && allSame) {
+      setTenzies(true)
+    }
+  }, [diceNumbers])
+
   return (
     <main className='container'>
+      {tenzies && <Confetti />}
       <h1>Tenzies</h1>
       <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className='dice-container'>
         {diceElements}
       </div>
-      <button onClick={getNewDice} className='roll-btn'>Roll</button>
+      <button 
+        onClick={getNewDice} 
+        className='roll-btn'
+      >
+          { tenzies ? 'New Game' : 'Roll' }
+      </button>
     </main>
   )
 }
